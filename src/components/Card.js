e12df51dc548a765a82c797d5acbea377902629e
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-10-26 21:00:11 -0400

    added data to file, along with ids for each question

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 // Card component is a card with a trivia question and multiple choice answers
 
-import React from "react";
+import React, { UseState } from "react";
 
-function Card() {
+
+const Card = (props) => {
+
+
     return (
         <div className="card">
-            THIS IS A CARD
+
         </div>
     );
 };
