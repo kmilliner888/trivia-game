ab22999d8035cad37ac3f09add2ca6f256c8b820
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-10-29 09:58:15 -0400

    handleCardOnClick function advances to next card in array, working on stopping at end of trivia round array to create 1 round that does not repeat

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,8 @@
 // Game component houses a deck (Deck component) of 10 random triva question cards per round,
 // along with answer and score responses
 
+/*eslint-disable-next-line no-unused-expressions*/
+
 import React, { useState } from "react";
 import Card from "./Card";
 import data from '../data/data';
@@ -9,6 +11,7 @@ import { Link } from 'react-router-dom';
 
 
 const Game = () => {
+    //Create a shuffle function to randomize the questions in the data array
     function shuffleArray(array) {
         for (var i = array.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
@@ -19,10 +22,26 @@ const Game = () => {
         };
     };
 
+    // Shuffle the data array and set it to cardData state
     const [cardData, setCardData] = useState(shuffleArray(data));
+    const [card, setCard] = useState(0);
     console.log('cardData', cardData);
+    console.log('card', card);
 
-    //Round consists of random 10 cards/objects from data array
+    const handleCardOnClick = () => {
+        setCard(prev => {
+            if (prev === triviaRound.length-1) {
+                return 0;
+            } else {
+                return prev + 1;
+            }
+        });
+    };
+
+    //Round consists of random 10 cards/objects from cardData array
+    // Create triviaRound which is set to randomized data array
+    // Limits number of cards to 10, then maps over the array
+    // 10 Card components created for the triviaRound
     let triviaRound = cardData.slice(0,10).map((card) => {
         return <Card
         key={card.id}
@@ -34,12 +53,12 @@ const Game = () => {
         />
     });
 
-    console.log('triviaRound', triviaRound);
-
     return (
         <div className="game">
-            {triviaRound}
-            <button className="button">NEXT</button>
+            <div>
+                {triviaRound[card]}
+            </div>
+            <button className="button" onClick={handleCardOnClick}>NEXT</button>
             <Link to='/'><button className="button">HOME</button></Link>
         </div>
     );
